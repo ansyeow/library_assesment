9906a008b70309daa82495e5f110b789e6cb5c3c
Andrew SY Ngee <andrew.sy.ngee@prubsn.com.my>
2023-09-18 10:23:57 +0800

    test

--- src/test/java/ansyeow/library/borrower/BorrowerIntegrationTest.java
+++ src/test/java/ansyeow/library/borrower/BorrowerIntegrationTest.java
@@ -1,17 +1,23 @@
 package ansyeow.library.borrower;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BorrowerIntegrationTest {
+
+    @Value(value="${local.server.port}")
+    private int port;
+
     @Test
     public void givenValidBody_whenRegisterNewBorrower_thenReceiveNewBorrowerId() {
-        given().body("""
+        given() .baseUri("http://localhost:" + port + "/")
+                .body("""
                         {"name":"Andrew", "emailAddress":"andrew@yahoo.com"}
                         """).
         when(). post("/borrower").
